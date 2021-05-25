9506e22798593dbcfce170bec9ea287c161e264b
Christian Car <carchristianjavier@gmail.com>
2021-05-25 12:47:28 -0600

    Export to files option have been added

--- Lab9.py
+++ Lab9.py
@@ -1,13 +1,43 @@
+import os
+from datetime import datetime
+
 loop=True
 equipment={"Switch":[], "Router":[], "Firewall":[]}
+options=["1","2","3","4","5"]
+
+def export():
+    date=str(datetime.now())
+    filer = open("Routers.txt","w+")
+    files = open("Switches.txt","w+")
+    filef = open("Firewalls.txt","w+")
+    print("***********Exportando archivos**************")
+    files.write("**Switches en el sistema**"+date+os.linesep)
+    filer.write("**Routers en el sistema**"+date+os.linesep)
+    filef.write("**Firewalls en el sistema**"+date+os.linesep)
+    for i in equipment["Switch"]:
+        files.write(i+os.linesep)
+    for i in equipment["Router"]:
+        filer.write(i+os.linesep)
+    for i in equipment["Firewall"]:
+        filef.write(i+os.linesep)
+    files.close()
+    files.close()
+    filef.close()
+    print("***********Archivos exportados**************")
 
 while loop:
     print("Bienvenido, que desea ingresar?:")
     print("1- Switches.")
     print("2- Routers.")
     print("3- Firewall.")
-    print("4- Salir")
-    opt = int(input())
+    print("4- Exportar dispositivos a .txt")
+    print("5- Salir")
+    opt = input()
+    if opt in options:
+        opt = int (opt)
+    else:
+        print("Opcion incorrecta, vuelve a intentarlo")
+        continue
     name = ""
     
     if opt == 1:
@@ -23,7 +53,10 @@ while loop:
         name=input()
         equipment["Firewall"].append(name)
     elif opt == 4:
+        export()
+    elif opt == 5:
         loop=False
+
     print("***************************************************************************")
     print("SWITCHS EN EL SISTEMA")
     for i in equipment["Switch"]:
